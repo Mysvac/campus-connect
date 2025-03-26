e96b829a0b9c39eddeb526fc9bd1d86444306661
mysvac <mys_vac@hotmail.com>
2025-03-26 15:47:31 +0800

    添加后端基本结构

---
+++ backend/build.gradle.kts
@@ -0,0 +1,31 @@
+plugins {
+    java
+    id("org.springframework.boot") version "3.4.4"
+    id("io.spring.dependency-management") version "1.1.7"
+}
+
+group = "com.mythovac"
+version = "0.0.1-SNAPSHOT"
+
+java {
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(21)
+    }
+}
+
+repositories {
+    maven { url = uri("https://maven.aliyun.com/nexus/content/repositories/google") }
+    maven { url = uri("https://maven.aliyun.com/nexus/content/groups/public") }
+    google()
+    mavenCentral()
+}
+
+dependencies {
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+}
---
+++ backend/src/test/java/com/mythovac/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mythovac.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
