fa73347a1eb5feb7938adec6471a2be1bedb072c
mysvac <mys_vac@hotmail.com>
2025-04-09 15:50:41 +0800

    user

--- backend/src/main/java/com/mythovac/backend/mapper/UserMapper.java
+++ backend/src/main/java/com/mythovac/backend/mapper/UserMapper.java
@@ -1,4 +1,32 @@
 package com.mythovac.backend.mapper;
 
-public class UserMapper {
+import com.mythovac.backend.entity.User;
+import org.apache.ibatis.annotations.*;
+
+import java.util.List;
+
+@Mapper
+public interface UserMapper {
+    @Select("SELECT * FROM users")
+    List<User> getAllUsers();
+
+    @Select("SELECT * FROM users WHERE uid = #{uid}")
+    User getUserById(Long uid);
+
+    @Select("SELECT * FROM users WHERE phone = #{phone}")
+    User getUserByPhone(String phone);
+
+    @Insert("INSERT INTO users(uid, permission, phone, password, wallet, nickname, gender, email, profile, image)" +
+            " VALUES(#{permission}, #{phone}, #{password}, #{wallet}, #{nickname}, #{gender}, #{email}, #{profile}, #{image})")
+    @Options(useGeneratedKeys = true, keyProperty = "uid")
+    void insertUser(User user);
+
+    @Update("UPDATE users SET permission = #{permission}, phone = #{phone}," +
+            " password = #{password}, wallet = #{wallet}, nickname = #{nickname}, " +
+            " gender = #{gender}, email = #{email}, profile = #{profile}, " +
+            " image = #{image} WHERE user_id = #{userId}")
+    void updateUser(User user);
+
+    @Delete("DELETE FROM users WHERE uid = #{uid}")
+    void deleteUser(Long uid);
 }
